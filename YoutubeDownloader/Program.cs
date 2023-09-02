d2940b0d6d78cc9392506ee46ff2b379345192f1
Leon68 <leon-dev@ukr.net>
2023-09-02 23:38:58 +0300

    add codes

--- YoutubeDownloader/Program.cs
+++ YoutubeDownloader/Program.cs
@@ -37,6 +37,7 @@ namespace YoutubeDownloader
                 Console.WriteLine("An error occurred while downloading the videos: " + ex.Message);
             }
         }
+
         static async Task DownloadYouTubeVideo(string videoUrl, string outputDirectory)
         {
             var youtube = new YoutubeClient();
@@ -61,13 +62,9 @@ namespace YoutubeDownloader
 
             int indexParse = 0;
             if (int.TryParse(indexStr, out indexParse))
-            {
                 index = indexParse;
-            }
             else
-            {
                 index = 0;
-            }
 
             if (index >= muxedStreams.Count)
                 index = 0;
@@ -129,61 +126,4 @@ namespace YoutubeDownloader
         }
     }
 
-    public static class HttpClientExtensions
-    {
-        public static async Task DownloadAsync(this HttpClient client, 
-            string requestUri, 
-            Stream destination, 
-            IProgress<float>? progress = null, 
-            CancellationToken cancellationToken = default)
-        {
-            using (var response = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead))
-            {
-                var contentLength = response.Content.Headers.ContentLength;
-                using (var download = await response.Content.ReadAsStreamAsync())
-                {
-                    if (progress == null || !contentLength.HasValue)
-                    {
-                        await download.CopyToAsync(destination);
-                        return;
-                    }
-
-                    var relativeProgress = new Progress<long>(totalBytes => progress.Report((float)totalBytes / contentLength.Value));
-                    await download.CopyToAsync(destination, 81920, relativeProgress, cancellationToken);
-                    progress.Report(1);
-                }
-            }
-        }
-    }
-
-    public static class StreamExtensions
-    {
-        public static async Task CopyToAsync(this Stream source, 
-            Stream destination, 
-            int bufferSize, 
-            IProgress<long>? progress = null, 
-            CancellationToken cancellationToken = default)
-        {
-            if (source == null)
-                throw new ArgumentNullException(nameof(source));
-            if (!source.CanRead)
-                throw new ArgumentException("Has to be readable", nameof(source));
-            if (destination == null)
-                throw new ArgumentNullException(nameof(destination));
-            if (!destination.CanWrite)
-                throw new ArgumentException("Has to be writable", nameof(destination));
-            if (bufferSize < 0)
-                throw new ArgumentOutOfRangeException(nameof(bufferSize));
-
-            var buffer = new byte[bufferSize];
-            long totalBytesRead = 0;
-            int bytesRead;
-            while ((bytesRead = await source.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) != 0)
-            {
-                await destination.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
-                totalBytesRead += bytesRead;
-                progress?.Report(totalBytesRead);
-            }
-        }
-    }
 }
