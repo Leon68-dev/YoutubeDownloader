2ec25fa5d252f38b39e9e4948c3b47912b02f6dd
Leon68 <leon-dev@ukr.net>
2023-09-02 23:29:58 +0300

    add codes

--- YoutubeDownloader/Program.cs
+++ YoutubeDownloader/Program.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Web;
 using System.Xml;
 using YoutubeExplode;
+using YoutubeExplode.Videos;
 
 namespace YoutubeDownloader
 {
@@ -44,17 +45,46 @@ namespace YoutubeDownloader
             string sanitizedTitle = string.Join("_", video.Title.Split(Path.GetInvalidFileNameChars()));
             var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
             var muxedStreams = streamManifest.GetMuxedStreams().OrderByDescending(s => s.VideoQuality).ToList();
+
+            Console.WriteLine("Video quality");
+            Console.WriteLine("----------------------------------------------------------------");
+            int index = 0;
+            foreach(var item in muxedStreams)
+            {
+                Console.WriteLine($"index: {index} - {item.VideoQuality.Label} ({item.Container.Name}) {(int)item.Size.MegaBytes}MB");
+                Console.WriteLine("----------------------------------------------------------------");
+                index++;
+            }
+
+            Console.Write("Please select index: ");
+            string indexStr = Console.ReadLine() ?? "0";
+
+            int indexParse = 0;
+            if (int.TryParse(indexStr, out indexParse))
+            {
+                index = indexParse;
+            }
+            else
+            {
+                index = 0;
+            }
+
+            if (index >= muxedStreams.Count)
+                index = 0;
+
             var prog = new Progress<KeyValuePair<long, long>>();
 
             if (muxedStreams.Any())
             {
                 Console.WriteLine("Download started");
-
-                var streamInfo = muxedStreams.First();
+                
+                moveTop += (muxedStreams.Count * 2) + 2;
+                var streamInfo = muxedStreams[index];
+                
                 //using var httpClient = new HttpClient();
                 //var stream = await httpClient.GetStreamAsync(streamInfo.Url);
                 var datetime = DateTime.Now;
-                string outputFilePath = Path.Combine(outputDirectory, $"{sanitizedTitle}.{streamInfo.Container}");
+                string outputFilePath = Path.Combine(outputDirectory, $"{sanitizedTitle}_{streamInfo.VideoQuality.Label}.{streamInfo.Container}");
                 //using var outputStream = File.Create(outputFilePath);
                 //await stream.CopyToAsync(outputStream);
 
@@ -80,6 +110,7 @@ namespace YoutubeDownloader
         }
 
         static int clc = 0;
+        static int moveTop = 2;
         private static void Progress_ProgressChanged(object? sender, float e)
         {
             int prc = (int)(e * 100);
@@ -87,11 +118,11 @@ namespace YoutubeDownloader
             {
                 lock(locker)
                 {
-                    Console.SetCursorPosition(0, 2);
+                    Console.SetCursorPosition(0, moveTop);
                     Console.WriteLine($"                               ");
-                    Console.SetCursorPosition(0, 2);
+                    Console.SetCursorPosition(0, moveTop);
                     Console.WriteLine($"Downloading...{prc}%");
-                    Console.SetCursorPosition(0, 2);
+                    Console.SetCursorPosition(0, moveTop);
                 }
             }
             clc = prc;
